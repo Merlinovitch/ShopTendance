4d22fa91cd363080dbb8a73fda85cfd2b36b3b11
Alexandre Merlin <a.merlin92@gmail.com>
2023-10-23 10:20:17 +0200

    navbar + burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,36 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <ul className="navbar">
-        <li>
+    <>
+      <section className="top-nav">
+        <div className="logo">
           <h1>
             <Link to="/">Shop Tendance</Link>
           </h1>
-        </li>
-        <li>
-          <Link to="/Hommes">Hommes</Link>
-        </li>
-        <li>
-          <Link to="/Femmes">Femmes</Link>
-        </li>
-        <li>
-          <Link to="/Enfants">Enfants</Link>
-        </li>
-        <li>
-          <Link to="/Ados">Ados</Link>
-        </li>
-        <li>
-          <Link to="/Bebes">Bebes</Link>
-        </li>
-      </ul>
-    </div>
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label className="menu-button-container" for="menu-toggle">
+          <div className="menu-button"></div>
+        </label>
+        <ul className="menu">
+          <li>
+            <Link to="/Hommes">Hommes</Link>
+          </li>
+          <li>
+            <Link to="/Femmes">Femmes</Link>
+          </li>
+          <li>
+            <Link to="/Enfants">Enfants</Link>
+          </li>
+          <li>
+            <Link to="/Ados">Ados</Link>
+          </li>
+          <li>
+            <Link to="/Bebes">Bebes</Link>
+          </li>
+        </ul>
+      </section>
+    </>
   );
 };
 
